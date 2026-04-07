00384f2a093ee770d585cb53c551f518584936c3
jayasaiprakash4413 <jaya.saiprakash@rupeek.com>
2026-04-07 17:43:08 +0530

    Refactor loan details lookup with column mapping

--- Field_team_report.py
+++ Field_team_report.py
@@ -249,11 +249,10 @@ scope = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_file(
-    "credentials.json",
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
     scopes=scope
 )
-
 client = gspread.authorize(creds)
 
 # ---------------------------
